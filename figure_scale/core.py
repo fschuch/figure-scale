c598c320c0bd1260a3aecb1041443d7c5eaeec42
Felipe N. Schuch <me@fschuch.com>
2023-09-03 19:34:37 -0300

    add initial functionality

--- figure_scale/core.py
+++ figure_scale/core.py
@@ -1,28 +1,75 @@
-"""Module containing the core functionality of the demo project."""
+"""Module containing the core functionality of the project."""
+from __future__ import annotations
+
+import functools
+from collections.abc import Sequence
+from contextlib import contextmanager
+from dataclasses import dataclass, replace
+from typing import NamedTuple
+
+from matplotlib.pyplot import rc_context
+
+from figure_scale.unit_conversion import CONVERSION_TABLE
 
 
-def demo_function(incoming_int: int) -> int:
-    """Demo function that doubles the input.
+class FigSize(NamedTuple):
+    """A named tuple to hold figure size information."""
 
-    Args
-    ----
-    incoming_int: The input to be doubled.
+    width: float
+    height: float
 
-    Returns
-    -------
-    The doubled input.
 
-    Raises
-    ------
-    TypeError: If the input is not an integer.
+GOLDEN_RATIO = (5.0**0.5 - 1.0) / 2.0
+"""The golden ratio."""
 
-    Examples
-    --------
-    >>> demo_function(2)
-    4
-    >>> demo_function(3)
-    6
-    """
-    if isinstance(incoming_int, int) is False:
-        raise TypeError(f"Input must be an integer, got {type(incoming_int)}.")
-    return incoming_int * 2
+
+@dataclass(frozen=True)
+class FigureScale(Sequence):
+    """Class to hold figure scale information."""
+
+    width: float
+    height_abs: float | None = None
+    height_rel: float = GOLDEN_RATIO
+    units: str = "in"
+
+    def __post_init__(self):
+        """Validate the values."""
+        if self.units not in CONVERSION_TABLE:
+            raise ValueError(
+                "Unknown unit: {}. The available options are: {}".format(
+                    self.units, ", ".join(CONVERSION_TABLE.keys())
+                )
+            )
+
+    def replace(self, **kwargs) -> FigureScale:
+        """Replace the attributes of the figure scale."""
+        return replace(self, **kwargs)
+
+    @contextmanager
+    def __call__(self, **kwargs):
+        """Replace the attributes of the figure scale."""
+        with rc_context({"figure.figsize": self, **kwargs}):
+            yield
+
+    def __getitem__(self, index: slice | int):
+        """Get the figure size."""
+        return self._fig_size[index]
+
+    def __len__(self) -> int:
+        """Return the length of the figure size."""
+        return len(self._fig_size)
+
+    @functools.cached_property
+    def _fig_size(self) -> FigSize:
+        """Calculate the figure size."""
+        if self.height_abs is None:
+            height_abs = self.width * self.height_rel
+        else:
+            height_abs = self.height_abs
+
+        try:
+            factor = CONVERSION_TABLE[self.units]
+        except KeyError as err:
+            raise ValueError(f"Unknown unit: {self.units}. ") from err
+
+        return FigSize(self.width * factor, height_abs * factor)
---
+++ tests/test_unit_conversion.py
@@ -0,0 +1,46 @@
+"""Test the unit conversion table."""
+import pint
+import pytest
+
+from figure_scale.unit_conversion import (
+    CONVERSION_TABLE,
+    update_conversion_table,
+)
+
+
+@pytest.mark.parametrize("unit", CONVERSION_TABLE)
+def test_convert_unit(unit):
+    """Test that the conversion table is correct."""
+    if unit == "pt":
+        return
+    ureg = pint.UnitRegistry()
+    actual_value = 1.0 * CONVERSION_TABLE[unit]
+    expected_value = ureg(unit).to("in").magnitude
+    relative_error = abs(actual_value - expected_value) / expected_value
+    assert relative_error < 1e-8
+
+
+def test_convert_unit__pt():
+    """Test that the conversion table is correct."""
+    actual_value = 72.0 * CONVERSION_TABLE["pt"]
+    expected_value = 1.0
+    relative_error = abs(actual_value - expected_value) / expected_value
+    assert relative_error < 1e-8
+
+
+def test_update_conversion_table__success():
+    """Test that the conversion table is correct."""
+    update_conversion_table(test=1.0)
+    assert CONVERSION_TABLE["test"] == 1.0
+
+
+def test_update_conversion_table__not_float():
+    """Test that the conversion table is correct."""
+    with pytest.raises(TypeError):
+        update_conversion_table(test="1.0")
+
+
+def test_update_conversion_table__overwrite():
+    """Test that the conversion table is correct."""
+    with pytest.raises(ValueError):
+        update_conversion_table(ft=1.0)
