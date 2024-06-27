0f66c89c6826457fea7ebb5efbaf52613ffc26e7
Felipe N. Schuch <me@fschuch.com>
2024-06-27 18:34:47 -0300

    fix: internal version file

--- figure_scale/__init__.py
+++ figure_scale/__init__.py
@@ -1,5 +1,5 @@
 """Tweak the scale of figures in matplotlib."""
 
+from figure_scale._version import __version__
 from figure_scale.core import FigureScale
 from figure_scale.unit_conversion import UnitConversionMapping
-from figure_scale.version import __version__
